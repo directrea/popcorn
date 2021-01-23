c13352490d6f79348b36e5894e9decdbf391ac87
root <ne191209@senshu-u.jp>
2021-01-23 13:59:00 +0000

    herokuにアップ

--- db/seeds/production/performances.rb
+++ db/seeds/production/performances.rb
@@ -1,14 +1,16 @@
-date = %w(2021-01-01 2021-01-02 2021-01-03)
+today = Date.today
+date = [today+1.day,today+2.day,today+3.day]
 stime = ['12:00','15:00','18:00']
 etime = %w(13:30 16:30 19:30)
 
-1.upto(10) do |idx|
+1.upto(7) do |idx|
     0.upto(2) do |n|
         Performance.create(
             movie_id: idx,
             screening_date: date[n],
             start_time: stime[n],
-            end_time: etime[n]
+            end_time: etime[n],
+            screen: n+1
         )
     end
 end
